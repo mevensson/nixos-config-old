7829cbbd742779dfe23035a66ad0150dc29bcfb7
Mattias Evensson <mattias@evensson.eu>
2019-09-29 10:20:40 +0200

    Fix printing

--- modules/printing.nix
+++ modules/printing.nix
@@ -2,9 +2,16 @@
 
 {
   services = {
+    # Enable Avahi service discovery demon
+    avahi = {
+      enable = true;
+      nssmdns = true;
+    };
+
     # Enable CUPS to print documents.
     printing = {
       enable = true;
+      drivers = [ pkgs.hplip ];
     };
   };
 }
